a93f84d189d50c550c5ffd8c64354e8baef3016b
seonghyeon2471 <carino@hanyang.ac.kr>
2026-03-24 13:36:29 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -139,15 +139,15 @@ def play_next(ctx):
     url = song_queue.pop(0)
 
     ydl_opts = {
-        'format': 'bestaudio[ext=m4a]/bestaudio/best',
-        'noplaylist': True,
-        'quiet': True,
-        'nocheckcertificate': True,
-        'ignoreerrors': True,
-        'no_warnings': True,
-        'default_search': 'auto',
-        'source_address': '0.0.0.0',
-        'cookiefile': 'cookies.txt'
+    'format': 'bestaudio/best',
+    'noplaylist': True,
+    'quiet': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',
+    'cookiefile': 'cookies.txt'
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
